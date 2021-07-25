faab12d3eb2551a4a7fe9080262a5d9748a0d1ea
sakebook <sakebook@gmail.com>
2021-07-26 01:18:45 +0900

    Update major version to 1.0.0

--- Formula/pbssc.rb
+++ Formula/pbssc.rb
@@ -1,8 +1,8 @@
 class Pbssc < Formula
   desc "You can get the device image from the CLI and copy it to the clipboard"
   homepage "https://github.com/sakebook/pbssc"
-  url "https://github.com/sakebook/pbssc/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "cd7b1a2e50ed0e826b553d648fb9e3fd87e472d1b744b84ff0ed7c9b66e8a892"
+  url "https://github.com/sakebook/pbssc/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "75d0fb8686e20ae4e5bee7e34ed1ceda1a2fe8382c0fea693bfe210ec2a4c5ad"
   license "Apache-2.0"
 
   def install
