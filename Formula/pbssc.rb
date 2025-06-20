693bec0957dd14528ed46092ff49d66eefa86339
sakebook <sakebook@gmail.com>
2025-06-20 11:55:14 +0900

    Update v1.0.1

--- Formula/pbssc.rb
+++ Formula/pbssc.rb
@@ -1,8 +1,8 @@
 class Pbssc < Formula
   desc "You can get the device image from the CLI and copy it to the clipboard"
   homepage "https://github.com/sakebook/pbssc"
-  url "https://github.com/sakebook/pbssc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "75d0fb8686e20ae4e5bee7e34ed1ceda1a2fe8382c0fea693bfe210ec2a4c5ad"
+  url "https://github.com/sakebook/pbssc/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "184230da10caf36922a27e18eeed66e91c52fc0065cb91ef9fac60a34501ab13"
   license "Apache-2.0"
 
   def install
@@ -10,6 +10,7 @@ class Pbssc < Formula
   end
 
   test do
-    system "false"
+    ENV["PBSSC_IMAGE_PATH"] = testpath/"dummy_image_path"
+    system bin/"pbssc", "-h"
   end
 end
